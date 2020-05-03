ce9134185441148b4ebc4cc07b4bfb25fe0118ba
MarcoNewman <marcoanewman@gmail.com>
2020-05-03 18:05:07 -0400

    stylized merchange qr scanner

--- screens/merchant/ScanScreen.js
+++ screens/merchant/ScanScreen.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, Button } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
+import Colors from '../../constants/Colors';
 
 const ScanScreen = props => {
     const [hasPermission, setHasPermission] = useState(null);
@@ -10,36 +11,47 @@ const ScanScreen = props => {
         (async () => {
         const { status } = await BarCodeScanner.requestPermissionsAsync();
         setHasPermission(status === 'granted');
-    })();
-  }, []);
+        })();
+    }, []);
 
-  const handleBarCodeScanned = ({ type, data }) => {
-    setScanned(true);
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-  };
+    const handleBarCodeScanned = ({ type, data }) => {
+        setScanned(true);
+        alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    };
 
-  if (hasPermission === null) {
-    return <Text>Requesting for camera permission</Text>;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
+    if (hasPermission === null) {
+        return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
    
     return (
-        <View>
+        <View style={styles.screen}>
             <Text>
-                This is the Scan Screen!
+                Scan a customer QR Code to credit 1 point
             </Text>
-            <BarCodeScanner
+            <BarCodeScanner style={styles.scanner}
             onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-            style={StyleSheet.absoluteFillObject}
-        />
-
-        {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+            barCodeTypes={[BarCodeScanner.Constants.BarCodeType.qr]}
+            />
+            {scanned && <Button title='New Customer' color={Colors.backgrounddark} onPress={() => setScanned(false)} />}
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen:{
+    flex:1,
+    alignItems:'center',
+    //justifyContent:'center'
+  },
+  scanner:{
+    ...StyleSheet.absoluteFill,
+    height:480,
+    top:20
+
+  }
+});
 
 export default ScanScreen;
