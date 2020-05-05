5095e31ec417afb3081e9b6438a6acf29f0a85c1
MarcoNewman <marcoanewman@gmail.com>
2020-05-05 12:39:58 -0400

    cleaned up deal list styling

--- components/DealList.js
+++ components/DealList.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { StyleSheet, FlatList, Text, View } from 'react-native';
+import { StyleSheet, FlatList, SafeAreaView,Text, View } from 'react-native';
 import Colors from '../constants/Colors';
 import DealItem from './DealItem';
 import RewardBalance from './RewardBalance';
 
 
 const DealList = props => {
-    
-
     const tap = (ammount) => {
         if(props.merchantSide){
             props.navigation.navigate('ScanScreen', {
@@ -23,24 +21,23 @@ const DealList = props => {
     const renderDeal = itemData =>{
         console.log('render');
         return(
-            <View style={styles.itemContainer}>
-                <DealItem 
-                    style={styles.listItem}
+            <View style={{height:70, marginLeft:10, marginRight:10, alignContent:'center'}}>
+                <DealItem
                     title={itemData.item.reward}
                     onClick={tap.bind(this, itemData.item.ammount)}
                     color={Colors.background}
                 >
-                    <View style={styles.textContainer}>
+                    <View>
                         <RewardBalance 
                             balance={itemData.item.ammount}
-                            size={12}
-                        
-                    />
+                            size={10}
+                        />
                     </View>
                 </DealItem>
             </View>
         );
     };
+    
     return (
         <FlatList 
             data={props.dealData}
@@ -48,27 +45,7 @@ const DealList = props => {
             keyExtractor={(item, index) => item.reward}
             ListFooterComponent={props.footer}
         />
-        
     );
 };
-const styles = StyleSheet.create({
-    listItem:{
-        //flex:1,
-        height:150
-    },
-    // textContainer:{
-    //     // borderWidth:1
-    // },
-    itemContainer:{
-        flex:1,
-        height:150
-    },
-    text:{
-        color:Colors.lines,
-        marginHorizontal:10,
-        textAlign:'center'
-
-    }
-});
 
 export default DealList;
