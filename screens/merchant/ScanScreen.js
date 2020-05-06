2527a8aa1aca10b2f33e4ae0cd08c4500dce0044
Martin Garin <ggarin@gmail.com>
2020-05-06 10:32:37 -0400

    Working redeem rewards, need to catch exceptions thrown by method when user doens;t have enought points.

--- screens/merchant/ScanScreen.js
+++ screens/merchant/ScanScreen.js
@@ -6,17 +6,19 @@ import * as userActions from '../../store/actions/user';
 import Colors from '../../constants/Colors';
 
 const ScanScreen = props => {
-    const ammount = props.navigation.state.params;
+    const [ammount, setAmmount] = useState(props.navigation.state.params);
     const [hasPermission, setHasPermission] = useState(null);
     const [scanned, setScanned] = useState(false);
     const dispatch = useDispatch();
     const r_id = useSelector(state => state.merchants.myMerchant.id);
 
     //const ammount = props.navigation.getParam('ammount');
-    
+    useEffect(()=>{
+        setAmmount(props.navigation.state.params);
+    },[props])
     console.log('__________ammount________');
-    if(!(ammount.props === undefined))
-        {console.log(ammount.props.Ammount);}
+    if(!(ammount.params === undefined))
+        {console.log(ammount.params.Ammount);}
     console.log(ammount);
     
     useEffect(() => {
@@ -27,31 +29,35 @@ const ScanScreen = props => {
     })();
   }, []);
 
-  const handleBarCodeScanned = ({ type, data }) => {
+  const handleBarCodeScanned = async ({ type, data })  => {
         setScanned(true);
-        if(!(ammount.props === undefined)){
+        if(!(ammount.params === undefined)){
             try{
-                console.log('_________Updating Rewards__________');
-                console.log('R_id: ' + r_id);
-                console.log('U_ID: ' + data); 
+                // console.log('_________Updating Rewards__________');
+                // console.log('R_id: ' + r_id);
+                // console.log('U_ID: ' + data); 
 
-                dispatch(userActions.updateRewards(r_id, data, -ammount.props.Ammount));
+                await dispatch(userActions.updateRewards(r_id, data, -ammount.params.Ammount))
+                setAmmount(0);
+                props.navigation.goBack();
+                
+                //need to handle error if user doesn't have enough rewards... 
             }catch(err){
                 console.log('there was an error')
             }
-            alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+            alert(`Deal Redeemd!!  \n${ammount.params.Ammount} Subtracted from User: ${data}!`);
 
         }else{
             try{
-                console.log('_________Updating Rewards__________');
-                console.log('R_id: ' + r_id);
-                console.log('U_ID: ' + data); 
+                // console.log('_________Updating Rewards__________');
+                // console.log('R_id: ' + r_id);
+                // console.log('U_ID: ' + data); 
 
-                dispatch(userActions.updateRewards(r_id, data, 1));
+                await dispatch(userActions.updateRewards(r_id, data, 1));
             }catch(err){
                 console.log('there was an error')
             }
-            alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+            alert(`1 POINT ADDED TO USER: ${data}!`);
         };
   };
   if (hasPermission === null) {
@@ -63,15 +69,12 @@ const ScanScreen = props => {
    
     return (
         <View style={styles.screen}>
-            <Text>
-                Scan a customer QR Code to credit rewards!
-            </Text>
             <BarCodeScanner style={styles.scanner}
             onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
             barCodeTypes={[BarCodeScanner.Constants.BarCodeType.qr]}
         />
 
-        {scanned && <Button title={'New Customer'} color={Colors.backgrounddark} onPress={() => setScanned(false)} />}
+        {scanned && <View style={styles.button}> <Button title={'Scan Again?'} color={Colors.backgrounddark} onPress={() => setScanned(false)} /> </View>}
         </View>
     );
 };
@@ -80,12 +83,18 @@ const styles = StyleSheet.create({
     screen:{
         flex:1,
         alignItems:'center',
+        justifyContent:'center'
     },
     scanner:{
         ...StyleSheet.absoluteFill,
         height:"95%",
         top:20
+    },
+    button:{
+        height:50,
+        width:100
     }
+
 });
 
 export default ScanScreen;
